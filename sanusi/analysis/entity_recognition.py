307bb02fa98bab12957ad25c760478e93b52c57c
dejetem <enyeoluwarotimisimon@gmail.com>
2025-07-07 14:34:43 +0100

    lazy-load the memory-heavy libraries

--- sanusi/analysis/entity_recognition.py
+++ sanusi/analysis/entity_recognition.py
@@ -1,32 +1,34 @@
-from rake_nltk import Rake
-import spacy
-
-nlp = spacy.load("en_core_web_sm")
-
+from functools import lru_cache
 
 def extract_keywords(text):
+    from rake_nltk import Rake
+
     r = Rake()
     r.extract_keywords_from_text(text)
     return r.get_ranked_phrases()
 
 
+@lru_cache(maxsize=1)
+def get_nlp():
+    import spacy
+    return spacy.load("en_core_web_sm")
+
+
 def extract_entities(text):
+    nlp = get_nlp()
     doc = nlp(text)
-    entities = [(ent.text, ent.label_) for ent in doc.ents]
-    return entities
+    return [(ent.text, ent.label_) for ent in doc.ents]
 
 
 def extract_topics(text):
     keywords = extract_keywords(text)
     entities = [ent[0] for ent in extract_entities(text)]
 
-    # Combine and remove duplicates for combined topics (if needed in future)
     combined = list(set(keywords + entities))
 
-    result = {
-        "keywords": list(set(keywords)),  # To ensure no duplicate keywords
-        "entities": list(set(entities)),  # To ensure no duplicate entities
+    return {
+        "keywords": list(set(keywords)), # To ensure no duplicate keywords
+        "entities": list(set(entities)), # To ensure no duplicate entities
         "combined": combined,
     }
 
-    return result
